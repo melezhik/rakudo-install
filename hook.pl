9168d407a0d2954f6e9d6eb04c7b43d16cda436b
Alexey Melezhik <melezhik@gmail.com>
2017-03-19 18:55:05 +0000

    add download story

--- hook.pl
+++ hook.pl
@@ -1,5 +1,8 @@
 if (os() =~ /centos/) {
-  run_story("/install/centos/");
+  my $url = config()->{rpm_url};
+  my $file = $url; s{.*/(.*)}[$1] for $file;
+  run_story("/install/centos/", { file => $file }) 
+    if run_story("/download", { url => $url, file => $file })->{status};
   set_stdout("done");
 } elsif ( os() =~ /debian/ ) {
   run_story("/install/debian");
