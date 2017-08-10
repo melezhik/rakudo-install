15a94ab6a56b452fd0ebbf85e93eb1246de02eba
Alexey Melezhik <melezhik@gmail.com>
2017-08-10 16:24:17 +0300

    fix install_zef_as_root bug

--- modules/install-zef/story.bash
+++ modules/install-zef/story.bash
@@ -5,7 +5,7 @@ export PATH=/opt/rakudo/bin:$PATH
 if test -f /opt/rakudo/share/perl6/bin/zef; then
   echo zef is already installed, nothing to do here ...
 else
-  install_zef_as_root.sh
+  install_zef_as_root
 fi
 
 zef --help 2>&1 |head -n 1
