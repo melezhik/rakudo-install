5371755be2434b2feddc562740d6ba350b414c23
Alexey Melezhik <melezhik@gmail.com>
2017-11-24 12:17:00 +0300

    alpine support

--- hook.pl
+++ hook.pl
@@ -1,17 +1,19 @@
-my $url = config()->{url};
+my $os  = os();
+my $url = config()->{url} || config()->{os_url}->{$os};
+
 my $file = $url; s{.*/(.*)}[$1] for $file;
 
 while (1){
 
-  if (os() =~ /(centos|debian|ubuntu)/) {
-    my $os = $1;  
+  if ($os =~ /(centos|debian|ubuntu|alpine)/) {
+    my $os_short = $1;  
     run_story("/download", { url => $url, file => $file })->{status} or last;
-    run_story("/install/$os/", { file => $file })->{status} or last; 
+    run_story("/install/$os_short/", { file => $file })->{status} or last; 
     run_story("/install-zef")->{status} or last; 
     set_stdout("done");
     last;
   } else {
-    set_stdout("os ".os()." not supported");
+    set_stdout("os $os not supported");
     last;
   }
   
