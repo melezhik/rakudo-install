3e4359feeb040d781c1775ede8e19c1ecbb32e35
Alexey Melezhik <melezhik@gmail.com>
2017-11-24 09:27:08 +0000

    alpine fixes

--- modules/install/alpine/story.bash
+++ modules/install/alpine/story.bash
@@ -1,13 +1,13 @@
 set -e
 
-if apk info -e rakudo 2>/dev/null; then
+if apk info -e rakudo-pkg 2>/dev/null; then
   echo 'looks like rakudo already installed, nothing to do here'
 else
   file=$(story_var file)
   apk add --allow-untrusted ~/.rakudo-cache/$file
 fi 
 
-PATH=/opt/rakudo/bin:$PATH perl6 --version
+PATH=/opt/rakudo-pkg/bin:$PATH perl6 --version
 
-apk version rakudo
-apk info -e rakudo
+apk version rakudo-pkg
+apk info -e rakudo-pkg
