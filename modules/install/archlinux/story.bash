bb9bc84d84007d824b4570f679d3a7c4b98a09d7
Alexey Melezhik <melezhik@gmail.com>
2017-11-28 13:54:32 +0300

    check if rakudo is installed  for arch

--- modules/install/archlinux/story.bash
+++ modules/install/archlinux/story.bash
@@ -2,17 +2,26 @@
 
 set -e
 
-repo=$(story_var repo)
-REPO_CONFIG="$cache_dir/pacman_rakudo.conf"
+if pacman -Q -q rakudo 2>/dev/null 1>/dev/null ; then
 
-cat << PACMAN_ADDITION_CONFIG > $REPO_CONFIG
+  echo 'looks like rakudo already installed, nothing to do here'
+
+else
+
+  repo=$(story_var repo)
+  REPO_CONFIG="$cache_dir/pacman_rakudo.conf"
+  
+  cat << PACMAN_ADDITION_CONFIG > $REPO_CONFIG
 Include = /etc/pacman.conf
 [rakudo]
 SigLevel = Optional
 Server = $repo
 PACMAN_ADDITION_CONFIG
+  
+  pacman -Syy --config $REPO_CONFIG
+  pacman -S --noconfirm --config $REPO_CONFIG rakudo
 
-pacman -Syy --config $REPO_CONFIG
-pacman -S --noconfirm --config $REPO_CONFIG rakudo
+fi
+
 
 perl6 -v
