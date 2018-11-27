4105a510c080d6e293d5b594ce9bc9e3affdf1e6
melezhik <melezhik@localhost.localdomain>
2018-11-26 22:25:40 -0600

    new version

--- Changes.md
+++ Changes.md
@@ -1,3 +1,7 @@
+# 0.2.4
+
+- Fix available versions list 
+
 # 0.2.1
 
 - List available distributions
--- modules/list-available/story.bash
+++ modules/list-available/story.bash
@@ -6,4 +6,4 @@ search=$(config search)
 
 set -x
 
-curl -s -k -f https://github.com/nxadm/rakudo-pkg/releases | grep download/v | perl -n -e '/href="(.*?)"/ and print $1, "\n"' | grep -v sha1 | grep -i $search
+curl -s -k -f https://github.com/nxadm/rakudo-pkg/releases | grep download/v | perl -n -e '/href="(.*?)"/ and print "https://github.com$1", "\n"' | grep -v sha1 | grep -i $search
